fbda425d8a450adabf3588fa2566dc590eac5656
Matt Severini <mseverini@mavenlink.com>
2019-11-11 21:00:26 -0700

    solution without classes

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,5 +1,68 @@
+require 'csv'
+
 class Greeting
-  def greet
-    "Hello, my friend."
+  def self.greet(name)
+    name = "my friend" if name.nil?
+
+    said, shouted = parse_commas(Array(name)).partition { |el| el.upcase != el }
+
+    join_greetings(said, shouted)
+  end
+
+  def self.join_greetings(said, shouted)
+    greeting = ""
+    greeting += say(said) if said.any?
+    greeting += " AND " if said.any? and shouted.any?
+    greeting += shout(shouted) if shouted.any?
+    greeting
+  end
+
+  def self.say(names)
+    opening = "Hello"
+    preposition = "and"
+    punctuation = "."
+    initial_join = ","
+
+    end_of_list = tail(names.length, final_name(preposition, names.last, punctuation), punctuation)
+    enunciate(names, opening, initial_join, end_of_list)
+  end
+
+  def self.shout(names)
+    opening = "HELLO"
+    preposition = "AND"
+    punctuation = "!"
+    initial_join = ""
+
+    end_of_list = tail(names.length, final_name(preposition, names.last, punctuation), punctuation)
+    enunciate(names, opening, initial_join, end_of_list)
+  end
+
+  def self.tail(num_names, final_name, punctuation)
+    case num_names
+    when 1
+      punctuation
+    when 2
+      " #{final_name}"
+    else
+      ", #{final_name}"
+    end
+  end
+
+  def self.final_name(preposition, final_name, punctuation)
+    "#{preposition} #{final_name}#{punctuation}"
+  end
+
+  def self.enunciate(names, opening, initial_join, end_of_list)
+    greeting = "#{opening}#{initial_join} #{names.first}"
+
+    names[1..-2].each do |name|
+      greeting += ", #{name}"
+    end
+
+    "#{greeting}#{end_of_list}"
+  end
+
+  def self.parse_commas(names)
+    names.flat_map{|name| CSV.parse(name)}.flatten.map{|name| name.strip}
   end
 end
--- spec/greeting_spec.rb
+++ spec/greeting_spec.rb
@@ -3,8 +3,46 @@ require_relative '../lib/greeting'
 
 RSpec.describe Greeting do
   describe "#greet" do
-    it "says hello to an old friend" do
-      expect(Greeting.new.greet).to eq "Hello, my friend."
+    subject { Greeting.greet name }
+    describe "when nil" do
+      let(:name) { nil }
+
+      it { is_expected.to eq "Hello, my friend." }
+    end
+
+    context "when a single name is said" do
+      let(:name) {"Bob"}
+      it {is_expected.to eq "Hello, Bob."}
+    end
+
+    context "when a single name is shouted" do
+      let(:name) {"JERRY"}
+      it {is_expected.to eq "HELLO JERRY!"}
+    end
+
+    context "when there are two said names" do
+      let(:name) {["Jill", "Jane"]}
+      it {is_expected.to eq "Hello, Jill and Jane."}
+    end
+
+    context "when there are multiple said names" do
+      let(:name) {["Amy", "Brian", "Charlotte"]}
+      it {is_expected.to eq "Hello, Amy, Brian, and Charlotte."}
+    end
+
+    context "when there are spoken and shouted names" do
+      let(:name) {["Amy", "BRIAN", "Charlotte"]}
+      it {is_expected.to eq "Hello, Amy and Charlotte. AND HELLO BRIAN!"}
+    end
+
+    context "when names are combined" do
+      let(:name) {["Bob", "Charlie, Dianne"]}
+      it {is_expected.to eq "Hello, Bob, Charlie, and Dianne."}
+    end
+
+    context "when names are combined" do
+      let(:name) {["Bob", "\"Charlie, Dianne\""]}
+      it {is_expected.to eq "Hello, Bob and Charlie, Dianne."}
     end
   end
 end
