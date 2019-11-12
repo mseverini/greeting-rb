7b8691f6af98043e98bef36d0bbf2f6fa444906e
Matt Severini <mseverini@mavenlink.com>
2019-11-11 21:45:55 -0700

    code golf

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -8,15 +8,11 @@ class Greeting
 
     said, shouted = parse_commas(Array(name)).partition { |el| el.upcase != el }
 
-    join_greetings(said, shouted)
-  end
-
-  def self.join_greetings(said, shouted)
     join =  (said.any? and shouted.any?) ? " AND " : ""
-    "#{Sayer.new(said).enunciate}#{join}#{Shouter.new(shouted).enunciate}"
+    Sayer.new(said).enunciate + join + Shouter.new(shouted).enunciate
   end
 
   def self.parse_commas(names)
-    names.flat_map{|name| CSV.parse(name)}.flatten.map{|name| name.strip}
+    names.map{|name| CSV.parse(name)}.flatten.map{|name| name.strip}
   end
 end
